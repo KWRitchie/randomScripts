a2be3ef52e6fc9584c2ded302c37054c4636ba57
root <root@ky13-desktop.ky13buildbox.com>
2017-11-27 13:36:11 -0500

    added if for iptables/rhel6

--- dropHosts.sh
+++ dropHosts.sh
@@ -4,14 +4,17 @@
 ## need to define yourIP var 
 yourIP=
 
-# determine if host is vers 6 or 7  
+# determine if host is vers 6 or 7; assume 6 if the following is null  
 linuxVers=$(cat /etc/redhat-release | grep 7) 
 
 # determine all ips currently connected and add drop rules to iptables (vers 6) or firewalld (vers 7)
+hosts=$(w -i | grep -vw $yourIP | awk '{print $3}' | sed 1,2d)
 if [[ ! -z $linuxVers ]]; then
-  hosts=$(w -i | grep -vw $yourIP | awk '{print $3}' | sed 1,2d)
   for ips in $hosts; do firewall-cmd --permanent --add-rich-rule="rule family='ipv4' source address=$ips reject" > /dev/null 2>&1; done
   for ips in $hosts; do firewall-cmd --add-rich-rule="rule family='ipv4' source address=$ips reject" > /dev/null 2>&1; done
+fi
+if [[ -z $linuxVers ]];
+  for ips in $hosts; do iptables -A INPUT -s $ips -j DROP; done
 fi
 
 # add $hosts to tcpwrappers hosts.deny to prevent them of reconnecting to any services
